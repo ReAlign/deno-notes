802eb725b5f773b7aa4e0a1d3728ed90ed940377
realign <realign@yeah.net>
2020-05-06 21:01:57 +0800

    re:init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,82 @@
+module.exports = {
+    // base: '/readme-blog/',
+    // learn-notes.realign.pro
+    title: 'Learn Notes', // Title for the site. This will be displayed in the navbar.
+    description: '学习记录 代码分享 经验总结 开发教程',
+    lange: 'zh-CN',
+    theme: require.resolve('./theme/'),
+    // theme: '@vuepress/blog',
+    markdown: {
+        plugins: [
+            // '@org/foo', // 等价于 @org/markdown-it-foo，如果对应的包存在
+            ['markdown-it-copy', {
+                // 提供你的选项
+                showCodeLanguage: true
+            }]
+        ]
+    },
+    themeConfig: {
+        dateFormat: 'YYYY-MM-DD',
+        nav: [{
+                text: 'Blog',
+                link: '/posts/',
+            },
+            {
+                text: 'Tags',
+                link: '/tag/',
+            },
+        ],
+        directories: [{
+                id: 'home',
+                dirname: '_posts',
+                path: '/',
+                layout: 'IndexHome'
+            },
+            {
+                id: 'post',
+                dirname: '_posts',
+                path: '/posts/',
+                itemPermalink: '/posts/:year/:month/:day/:slug'
+            },
+        ],
+        footer: {
+            contact: [{
+                type: 'github',
+                link: 'https://github.com/zhb333/readme-blog',
+            }],
+            copyright: [{
+                    text: '粤ICP备20016112号',
+                    link: 'http://beian.miit.gov.cn',
+                },
+                {
+                    text: 'MIT Licensed | Copyright © 2020-present forapi.cn',
+                    link: 'https://github.com/zhb333/readme-blog',
+                },
+            ]
+        },
+        comment: {
+            service: 'vssue',
+            owner: 'zhb333',
+            repo: 'readme-blog',
+            clientId: '',
+            clientSecret: '',
+        },
+
+        newsletter: {
+            title: '邮件订阅',
+            content: '请输入您的邮箱...',
+            submitText: '订阅',
+            endpoint: 'https://forapi.us19.list-manage.com/subscribe/post?u=ac82b3046426d14cad8f2a3ef&amp;id=ebfc431c44'
+        },
+
+        sitemap: {
+            hostname: 'http://forapi.cn'
+        },
+
+        feed: {
+            canonical_base: 'http://forapi.cn',
+        },
+
+        smoothScroll: true
+    }
+}
---
+++ docs/.vuepress/theme/.layouts/.IndexHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    //
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.home {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: $headerHeight;
+  bottom: 60px;
+  background: linear-gradient(-90deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px) 0% 0% / 50px 50px, linear-gradient(rgba(0, 0, 0, 0.1) 1px, transparent 1px) 0% 0% / 50px 50px;
+}
+</style>
